cd496116ed350497e3cb8b58b20abb53b5699ecd
Chaima <amirou.septiembre@gmail.com>
2024-10-14 17:49:36 +0200

    no seeee

--- Tema1Tarea1/EjExtra.js
+++ Tema1Tarea1/EjExtra.js
@@ -7,29 +7,34 @@ const rl = readline.createInterface({
 });
 
 rl.question('Introduce la anchura de la tableta: ', (ancho) => {
-    if(parseInt(ancho))//si es numero
+    const N = parseInt(ancho); 
+    if(!isNaN(N))//si es numero
     {
-        const N = parseInt(ancho); 
+        
 
         rl.question('Introduce la altura de la tableta: ', (altura) => {
             
-            if(parseInt(altura))
+            const M = parseInt(altura); 
+            if(!isNaN(M))
             {
-                const M = parseInt(altura); 
+                
                 rl.question('Introduce el numero de rectangulos deseados: ', (rectangulos) => {
                     
-                    if(parseInt(rectangulos))
+                    const K = parseInt(rectangulos);
+                    if(!isNaN(K))
                     {
-                        const K = parseInt(rectangulos);
+                        
                         if((K%N === 0 && K/N <=M) || (K%M === 0 && K/M <=N))
                         {
                             console.log("La tableta se puede partir de un solo corte en "+K+" rectangulos");
                         }else{
                             console.log("La tableta NO se puede partir de un solo corte en "+K+" rectangulos")
                         }
+                        rl.close();
 
                     }else{
                         console.log(rectangulos + " no es un numero");   
+                        rl.close();
                     }
 
                 });
@@ -42,5 +47,6 @@ rl.question('Introduce la anchura de la tableta: ', (ancho) => {
     }else
     {
         console.log(ancho +" no es un numero");
+        rl.close();
     }
 });
