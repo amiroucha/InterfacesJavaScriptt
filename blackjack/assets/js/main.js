7fb41f4dbb76b1b950792e83888efb67b32c4fe2
Chaima <1925046@alu.murciaeduca.es>
2024-10-19 14:47:26 +0200

    ya esta echo

--- blackjack/assets/js/main.js
+++ blackjack/assets/js/main.js
@@ -7,10 +7,10 @@ const btnPedir = document.querySelector('#btnPedir');
 const btnPasar = document.querySelector('#btnPasar');
 const btnNuevoJuego = document.querySelector('#btnNuevoJuego');
 console.log(btnPedir, btnPasar, btnNuevoJuego);
-
+//muestra los puntos del ordenador
 const puntosHTMLJugador = document.querySelector('.jugador small');
-//tengo que buscar el small de la clase jugador
-const puntosHTMLComputadora = document.querySelectorAll('.computadora small');
+const puntosHTMLComputadora = document.querySelectorAll('.computadora small')[0]; 
+//donde se enseñan las cartas
 const divCartasJugador = document.querySelector('#jugador-cartas');
 const divCartasComputadora = document.querySelector('#computadoras-cartas');
 
@@ -19,55 +19,51 @@ const divCartasComputadora = document.querySelector('#computadoras-cartas');
 // Funciones
 
 // La función devuelve un array con todas las cartas de una baraja de forma aleatoria
-//suffle para barajar las cartas
 function crearBarajaAleatoria() {
     let cartas = ['01C','02C','03C','04C','05C','06C','07C','08C','09C','10C','10JC','10QC','10KC',
                   '01D','02D','03D','04D','05D','06D','07D','08D','09D','10D','10JD','10QD','10KD',
                   '01P','02P','03P','04P','05P','06P','07P','08P','09P','10P','10JP','10QP','10KP',
                   '01T','02T','03T','04T','05T','06T','07T','08T','09T','10T','10JT','10QT','10KT'
                 ];
-
+    //con el metodo sort se mezclan aleatoriamente
     baraja = cartas.sort(() => Math.random() - 0.5);
 }
 
 const pedirCarta = () => {
-    if (baraja.length === 0) {
+    if (baraja.length === 0) {//si esta vacia avisa
         throw 'Baraja vacia'; 
     }
-    const carta = baraja.pop();
+    const carta = baraja.pop();//se extrae la ultima carta de la baraja, tras ser barajada
     return carta;
 }
 
 const valorCarta = (carta) => {
     const valor = carta.substring(0, carta.length - 1);
-
+    //calcula el valor
+    //01 = A // J Q K = 10 // si no el valor de la carta por si
     return (valor === '01') ? 1 : (isNaN(valor) ? 10 : valor * 1);
 }
 
 //turno del ordenador
 const turnoComputadora = (puntosMinimos) => {
-    
-    do
-    {
-        const carta = pedirCarta();
-        puntosComputadora = puntosComputadora + valorCarta(carta);
-        puntosHTMLComputadora[1].innerText = puntosComputadora;
+    if (puntosMinimos <= 21) {
+        do {
+            const carta = pedirCarta(); //cogemos una nueva carta
+            puntosComputadora += valorCarta(carta); //se actualiza el valor de los puntos
+            puntosHTMLComputadora.innerText = puntosComputadora;
 
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${carta}.png`; //carpeta donde estan las cartas
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append(imgCarta);
+            const imgCarta = document.createElement('img');
+            imgCarta.src = `assets/cartas/${carta}.png`; // Ruta de la imagen
+            imgCarta.classList.add('carta'); 
+            divCartasComputadora.append(imgCarta); //se añade la imagen de la carta
 
-        if(puntosMinimos > 21)
-        {
-            break;
-        }
+            //Si el jugador se ha pasado de los 21, la computadora ya no juega, ya gano
+            if (puntosMinimos > 21) break;
 
-
-    }while((puntosComputadora<puntosMinimos) && (puntosMinimos<21))
-
+        } while (puntosComputadora < puntosMinimos && puntosComputadora <= 21);
+    }
     setTimeout(() => {
-       // Meter aquí los alert para que se muestren después de mostrar las cartas de la computadora
+       //Alert para avisar de quien gano y perdio
         if(puntosJugador === puntosComputadora)
         {
             alert('Empatee');
@@ -84,8 +80,8 @@ const turnoComputadora = (puntosMinimos) => {
 // Eventos 
 
 btnPedir.addEventListener('click', () => {
-    const carta   = pedirCarta();
-    puntosJugador += valorCarta(carta);
+    const carta = pedirCarta();//pido la carta
+    puntosJugador += valorCarta(carta);//añado el valor de la carta a los puntos
     puntosHTMLJugador.innerText = puntosJugador;
 
     const imgCarta = document.createElement('img');
@@ -94,14 +90,15 @@ btnPedir.addEventListener('click', () => {
     divCartasJugador.append(imgCarta);
 
     if(puntosJugador > 21){
-
-        btnPedir.disabled   = true;
-        btnDetener.disabled = true;
+        //se desabilitan pedir y pasar porq ya ha excedido 21
+        btnPedir.disabled = true;
+        btnPasar.disabled = true;
         console.log("Perdiste");
         turnoComputadora(puntosJugador);
 
     }else if(puntosJugador === 21){
         
+        //se desabilitan pedir y pasar porq ya ha gaado        
         btnPedir.disabled = true;
         btnPasar.disabled = true;
         console.log("Ganaste");
@@ -110,28 +107,35 @@ btnPedir.addEventListener('click', () => {
 });
 
 btnPasar.addEventListener('click', () => {
+    turnoComputadora(puntosJugador);
     btnPasar.disabled = true;
     btnPedir.disabled = true;
-    turnoComputadora(puntosJugador);
+    //se desabilitan los botones cuando juega el ordenador 
 });
 
 btnNuevoJuego.addEventListener('click', () =>{
+   //creo la baraja
     crearBarajaAleatoria();
-
+    //Reinicia los puntos
     puntosComputadora= 0;
     puntosJugador= 0;
+    //actualiza los HTML
     puntosHTMLJugador.innerText = 0;
     puntosHTMLComputadora.innerText = 0;
+    //habilita los botones para jugar
     btnPasar.disabled= false;
     btnPedir.disabled= false;
-    divCartasJugador.innerHTML= '';
-    divCartasComputadora.innerHTML ='';
+    //PAra que las cartas no desaparezcan cuando se le da a nuevo
+
+    divCartasJugador.innerHTML= '<img class="carta" src="assets/cartas/red_back.png"">';
+    divCartasComputadora.innerHTML ='<img class="carta" src="assets/cartas/red_back.png">';
    
 });
 
 
 // Código principal
-
+//Llamar a baraja nueva para empezar
+crearBarajaAleatoria();
 
 
 
