d1049e1b97cc99e5eff5b5151b6e9d91217509c3
Chaima Amirou <1925046@alu.murciaeduca.es>
2024-10-23 20:35:36 +0200

    jhgjkkj

--- TypeScriptTema/TresEnRayaProfesor/app2.js
+++ TypeScriptTema/TresEnRayaProfesor/app2.js
@@ -1,36 +1,109 @@
+// Inicializa el estado del juego
 const gameState = {
-    board: Array(9).fill(''),// Crea un tablero de 9 celdas, inicialmente vacías
-    currentPlayer: 'X', //empieza jugando la x
-    isGameOver: false //bool de cuando termina el juego
-  }; 
+    board: Array(9).fill('-'),
+    botones: "", // Array que apuntan a cada uno de los botones del tablero
+    currentPlayer: 'X',
+    isGameOver: false
+  };
   
-  //variables donde referencia con html
-  const statusElement = document.getElementById('status');// Elemento para mostrar el estado del juego
-  const resetButton = document.getElementById('reset');  // Botón para reiniciar el juego
+  const boardElement = document.getElementById('board');
+  const statusElement = document.getElementById('status');
+  const resetButton = document.getElementById('reset');
   
-  function initGame(){
+  function initGame() {
     gameState.botones = document.querySelector('#board').querySelectorAll('button');
-    gameState.board.forEach(btn=> {
-        btn.addEventListener('click', handleCellClick);
-      });
-      resetButton.addEventListener('click', handleCellClick);
-  }
-
-  function handlerbtnClick(event){
-
-  }
-
-  //actualizo quien va a jugar
-  function handlerResetClick(event)
-  {
-    gameState.board = Array(9).fill('-');
     gameState.botones.forEach(btn => {
-        btn.innerText = '';
+      btn.addEventListener('click', handleBtnClick);
     });
 
+    resetButton.addEventListener('click', handlerResetClick);
+  }
+ 
+  function handleBtnClick(event){
+    
+    if (gameState.board[this.id]!= '-' || gameState.isGameOver) return;
+
+    gameState.board[this.id] = gameState.currentPlayer;
+    gameState.botones[this.id].innerText= gameState.currentPlayer;
+
+    
+
+    if (comprobarGanador()) {
+      gameState.isGameOver = true;
+      updateStatus();
+    } else if (esEmpate()) {
+      gameState.isGameOver = true;
+      statusElement.innerText=`Se ha EMPATADO`;
+    } else {
+      gameState.currentPlayer = gameState.currentPlayer === 'X' ? 'O' : 'X';
+      updateStatus();
+    }
+
+  }
+ 
+  // Actualiza el estado de quién es el siguiente en jugar
+  function updateStatus() {
+    if (!gameState.isGameOver) {
+      statusElement.innerText=`Turno del Jugador: ${gameState.currentPlayer}`;
+    } else {
+      statusElement.innerText=`El Jugador "${gameState.currentPlayer}" HA GANADO`;
+    }
+  }
+  
+  // Verifica si hay un ganador
+  function comprobarGanador() {
+    if ((gameState.board[0] != '-') && 
+        (gameState.board[0] == gameState.board[1]) && 
+        (gameState.board[1] == gameState.board[2])){
+      return true; 
+    }else if ((gameState.board[3] != '-') && 
+              (gameState.board[3] == gameState.board[4]) && 
+              (gameState.board[4] == gameState.board[5])){
+      return true;
+    }else if ((gameState.board[6] != '-') && 
+               (gameState.board[6] == gameState.board[7]) && 
+               (gameState.board[7] == gameState.board[8])){
+      return true;
+    }else if ((gameState.board[0] != '-') && 
+               (gameState.board[0] == gameState.board[3]) && 
+               (gameState.board[3] == gameState.board[6])){
+      return true;
+    }else if ((gameState.board[1] != '-') && 
+               (gameState.board[1] == gameState.board[4]) && 
+               (gameState.board[4] == gameState.board[7])){
+      return true;
+    }else if ((gameState.board[2] != '-') && 
+               (gameState.board[2] == gameState.board[5]) && 
+               (gameState.board[5] == gameState.board[8])){
+               return true;
+    }else if ((gameState.board[0] != '-') && 
+               (gameState.board[0] == gameState.board[4]) && 
+               (gameState.board[4] == gameState.board[8])){
+      return true;
+    }else if ((gameState.board[2] != '-') && 
+               (gameState.board[2] == gameState.board[4]) && 
+               (gameState.board[4] == gameState.board[6])){
+      return true;
+    } 
+    else {
+       return false;
+      }
+  }
+
+  function esEmpate() {
+    return gameState.board.every(valor => valor !== '-');
+  }
+
+  function handlerResetClick(event){
+    gameState.board = Array(9).fill('-');
+    gameState.botones.forEach(btn =>{
+      btn.innerText = '';
+    });
     gameState.currentPlayer = 'X';
     gameState.isGameOver = false;
     updateStatus();
-
   }
+  
+  // Inicializa el juego al cargar la página
 
+document.addEventListener('DOMContentLoaded',initGame);
