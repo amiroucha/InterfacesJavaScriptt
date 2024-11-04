91fb06ccb5f11316b9302f8ba66441d2ac0d76b7
Chaima Amirou <1925046@alu.murciaeduca.es>
2024-11-04 20:07:43 +0100

    react creado

---
+++ JavaScript/loginReact/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {  //Creamos la función App
+  return (
+    <>  // <>...</>Indicamos que vamos a escribir en varias líneas código HTML
+      <main>
+        <h1>Introducción a REACT</h1>
+      </main>
+    </>
+  )
+}
+
+
+export default App
