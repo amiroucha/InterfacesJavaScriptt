cc0013aed9f427758ecca4270549b1c5723113e1
chaima <amirou.septiembre@gmail.com>
2024-11-26 16:55:43 +0100

    AAAAAAAAAAAAAAAAAA

--- proyecto02_ReactNative_DI-master/proyecto02_ReactNative_DI-master/src/components/MyText.tsx
+++ proyecto02_ReactNative_DI-master/proyecto02_ReactNative_DI-master/src/components/MyText.tsx
@@ -8,7 +8,8 @@ interface Props extends TextProps{tamanio: 'h1' | 'h2' | 'h3'}
 
 export const MyText = ({children, tamanio,...rest}:Props) => {
     return(
-        <Text {...rest} numberOfLines={1} adjustsFontSizeToFit style={[tamanio === 'h1' ?GlobalStyles.h1:null,
+        <Text {...rest} numberOfLines={1} adjustsFontSizeToFit 
+        syle={[tamanio === 'h1' ?GlobalStyles.h1:null,
         tamanio === 'h2' ?GlobalStyles.h2:null,
         tamanio === 'h3' ?GlobalStyles.h3:null
         ]}>
